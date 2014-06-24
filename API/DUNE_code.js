70c5bdac3778ec142fc65a22902752bfabe5445c
Philippe Courtemanche Home <sigma.court@gmail.com>
2014-06-24 12:16:06 -0400

    Made ball animation

--- API/DUNE_code.js
+++ API/DUNE_code.js
@@ -1,91 +1,37 @@
-//Run this
-(function () {
+$(document).ready(function(){
 
-	function gameLoop () {
+	var stage;
 	
-	  window.requestAnimationFrame(gameLoop);
+	function letsDoThis(){
+		
+		stage = new createjs.Stage("board");
+		
 
-	  coin.update();
-	  coin.render();
+		var ball = new createjs.Shape();
+		ball.graphics.beginFill("#000000").drawCircle(0, 0, 50);
+		ball.x = 50;
+		ball.y = 200;
+		ball.addEventListener("click", handleClick);
+		//Pass a config object
+		createjs.Tween.get(ball, {loop:true}).to({x:450}, 3000).to({x:0}, 3000);
+
+		//The ticker sends a tick
+		createjs.Ticker.addEventListener("tick", onTick);
+
+
+		stage.addChild(ball);
+		
 	}
-	
-	function sprite (options) {
-	
-		var that = {},
-			frameIndex = 0,
-			tickCount = 0,
-			ticksPerFrame = options.ticksPerFrame || 0,
-			numberOfFrames = options.numberOfFrames || 1;
-		
-		that.context = options.context;
-		that.width = options.width;
-		that.height = options.height;
-		that.image = options.image;
-		
-		that.update = function () {
 
-            tickCount += 1;
-
-            if (tickCount > ticksPerFrame) {
-
-				tickCount = 0;
-				
-                // If the current frame index is in range
-                if (frameIndex < numberOfFrames - 1) {	
-                    // Go to the next frame
-                    frameIndex += 1;
-                } else {
-                    frameIndex = 0;
-                }
-            }
-        };
-		
-		that.render = function () {
-		
-		  // Clear the canvas
-		  that.context.clearRect(0, 0, that.width, that.height);
-		  
-		  // Draw the animation
-		  that.context.drawImage(
-		    that.image,
-		    frameIndex * that.width / numberOfFrames,
-		    0,
-		    that.width / numberOfFrames,
-		    that.height,
-		    0,
-		    0,
-		    that.width / numberOfFrames,
-		    that.height);
-		};
-		
-		return that;
+	function onTick(event){
+		stage.update();
 	}
-				
-	var coin,
-		coinImage,
-		canvas;					
 
-	// Get canvas
-	canvas = document.getElementById("myCanvas");
-	canvas.width = 1168;
-	canvas.height = 768;
-	
-	// Create sprite sheet
-	coinImage = new Image();	
-	coinImage.src = "../ART/images/coin-sprite-animation.png";
-	// Create sprite
-	coin = sprite({
-		context: canvas.getContext("2d"),
-		width: 1000,
-		height: 100,
-		image: coinImage,
-		numberOfFrames: 10,
-		ticksPerFrame: 4
-	});
-	
-	// Load sprite sheet
-	coinImage.addEventListener("load", gameLoop);
+	function handleClick(){
+		console.log("Clicked the ball");
+	}
+	letsDoThis();
 	
 
-} ());
-
+	
+});
