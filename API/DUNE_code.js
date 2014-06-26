faac95e1dc50023a9d566d069578c85cfb878a5d
Philippe Courtemanche Home <sigma.court@gmail.com>
2014-06-25 21:10:42 -0400

    YOu can move around the coin or the little ball

--- API/DUNE_code.js
+++ API/DUNE_code.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
 	var stage;
-	
-	letsDoThis(stage);
+	var coinSheet;
+	var coin;
+
+	letsDoThis();
 	
 
 	
@@ -11,6 +13,15 @@ $(document).ready(function(){
 function letsDoThis(){
 		
 		stage = new createjs.Stage("board");
+
+		/*Defining the coin*/
+		coinSheet = new createjs.SpriteSheet({framerate: 20, images: ["../ART/images/coin-sprite-animation.png"], frames: {width: 100, height: 100,count: 10, regX: 50, regY: 50}, animations: {spin: [0, 9, "spin"]}});
+		coin = new createjs.Sprite(coinSheet);
+		coin.gotoAndPlay("walk");
+		coin.x = 1366/2;
+		coin.y = 768/2;
+		coin.addEventListener("click", animateCoin);
+
 
 		/*DEFINING THE BALL*/
 		var ball = new createjs.Shape();
@@ -26,13 +37,44 @@ function letsDoThis(){
 		createjs.Ticker.addEventListener("tick", updateStage);
 		console.log("FPS: "+createjs.Ticker.getFPS());
 
+
 		stage.addChild(ball);
+		stage.addChild(coin);
 		
 		function animateBall(event){
-		console.log("Clicked the ball");
-		createjs.Tween.get(ball, {loop:false}).to({x:450}, 3000).to({x:50}, 3000);
-	}
+			console.log("Clicked the ball: ball.x =" +ball.x);
+			createjs.Tween.get(ball, {loop:false}).to({x:100, y:100}, 3000).to({x:ball.x, y:ball.y}, 3000);
+		}
+		function animateCoin(event){
+			
+			console.log("Clicked the coin");
+			
+			var oldX = coin.x;
+			var oldY = coin.y;
 
+
+			var sign1 = 1;
+			var sign2 = 1;
+			
+			if(Math.random() >= 0.5 ){
+				sign1 = -1;
+			} else {
+				sign1 = 1;
+			}
+			if(Math.random() >= 0.5 ){
+				sign2 = -1;
+			} else {
+				sign2 = 1;
+			}
+
+			var deltaX = Math.random()*400*sign1;
+			var deltaY = Math.random()*200*sign2;
+
+			createjs.Tween.get(coin, {loop:false}).to({x:oldX+deltaX, y: oldY+deltaY}, 500)
+			console.log("Going to: ");
+			console.log("x ="+(oldX+deltaX));
+			console.log("y ="+(oldY+deltaY));
+		}
 	}
 
 	function updateStage(event){
