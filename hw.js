e4af220565d103dc563ef1ef9e2ef79204fb338c
Philippe Courtemanche <dwmodem@gmail.com>
2018-06-03 22:47:04 +0100

    REMOVED USELESS EMACS STUFF

--- hw.js
+++ hw.js
@@ -1,5 +1,1 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
-
-Helle world
+var msg = "Helle world"
